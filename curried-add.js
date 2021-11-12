b4f7b57b0f47f6b0054aaedb515bc33a7a8bb413
Tuan Anh Le <anhtuanle2101@gmail.com>
2021-11-12 10:24:48 -0500

    curried-add

--- curried-add.js
+++ curried-add.js
@@ -1,5 +1,21 @@
 function curriedAdd(total) {
-
+    let sum = 0;
+    const helper = (num)=>{
+        if (num !== undefined){
+            sum+=num;
+            return helper;
+        }else{
+            return sum;
+        }
+    }
+    if (total !== undefined){
+        sum = total;
+        return helper;
+    }
+    else{
+        return sum;
+    }
 }
+
 
 module.exports = { curriedAdd };
